ef859d35bf01ff8db569b082fde631dc8e1e5c13
Josia <20210614@aloe.ulima.edu.pe>
2024-11-05 15:24:08 -0500

    ispassword

--- jewelry_app/lib/components/forms/large_text_form_field.dart
+++ jewelry_app/lib/components/forms/large_text_form_field.dart
@@ -4,18 +4,21 @@ class LargeTextFormField extends StatelessWidget {
   final String titulo;
   final TextEditingController? controller;
   final Function(String) onChanged;
+  final bool isPassword; // Nuevo parámetro
 
   const LargeTextFormField({
     super.key,
     required this.titulo,
     this.controller,
     required this.onChanged,
+    this.isPassword = false, // Valor por defecto
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      obscureText: isPassword, // Usar el parámetro para ocultar el texto
       decoration: InputDecoration(
         hintText: titulo,
         filled: true,
